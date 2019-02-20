b62b1d9c1cdf81bce6166ba0bb17955727c5a823
moosecake <piyanag90@gmail.com>
2019-02-20 01:01:07 -0800

    if conditions for Add photo

--- src/components/AddPhoto.js
+++ src/components/AddPhoto.js
@@ -2,18 +2,21 @@ import React from 'react';
 
 function AddPhoto(props){
     function addPost(evt){
-        props.addPhoto({
+        console.log('hello');
+        if(evt.target.elements.namedItem("imageLink").value && evt.target.elements.namedItem("ImageDesc").value)
+   {props.addPhoto({
              id:Number(new Date()), 
              src: evt.target.elements.namedItem("imageLink").value,
              desc:evt.target.elements.namedItem("ImageDesc").value
             });
-        evt.stopPropagation();
+        }
+        evt.preventDefault();
     }
     return <div className="addPhoto">
      <form onSubmit={(evt) => addPost(evt)}>
-     <input type="text" name="imageLink" placeholder="Source"/><br/>
-     <input type="text" name="ImageDesc" placeholder="Description"/><br/>
-     <button type="submit" name="SubmitBtn">Post</button>
+     <input type="text" name="imageLink" placeholder="Source" required/><br/>
+     <input type="text" name="ImageDesc" placeholder="Description" required/><br/>
+     <button  name="SubmitBtn">Post</button>
      </form>
 
     </div>
